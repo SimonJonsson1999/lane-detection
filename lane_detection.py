e4334c73cb2d5930d0f0e8fd77c33fdd53f79588
unknown <simon.jonsson1999@outlook.com>
2024-12-12 19:19:45 +0100

    Refactoring, add config file for constants

--- lane_detection.py
+++ lane_detection.py
@@ -4,45 +4,30 @@ import matplotlib.pyplot as plt
 
 
 class LaneLineDetector(object):
-    def __init__(self):
-        self.kernel_size = 5
-        self.low_threshold = 180
-        self.high_threshold = 240
-        self.apex_factor = 0.9
+    def __init__(self, config):
+        self.config = config
+        self.kernel_size = self.config['canny_edge'].get('kernel_size', 5)
+        self.low_threshold = self.config['canny_edge'].get('low_threshold', 180)
+        self.high_threshold = self.config['canny_edge'].get('high_threshold', 240)
+        self.apex_factor = self.config['region_of_interest'].get('apex_factor', 0.85)
+
+        # Hough transform parameters
+        self.rho = self.config['hough_transform'].get('rho', 1)
+        self.theta = self.config['hough_transform'].get('theta', np.pi / 180)
+        self.threshold = self.config['hough_transform'].get('threshold', 2)
+        self.min_line_length = self.config['hough_transform'].get('min_line_length', 4)
+        self.max_line_gap = self.config['hough_transform'].get('max_line_gap', 5)
 
     def detect(self, img):
-        left_bottom, right_bottom, apex = self._get_region_of_interest(img)
-        gray_img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-        blur_gray = cv2.GaussianBlur(gray_img,(self.kernel_size, self.kernel_size),0)
-        edges = cv2.Canny(blur_gray, self.low_threshold, self.high_threshold)
-
-        mask = np.zeros_like(edges)   
-        ignore_mask_color = 255
-        vertices = np.array([[left_bottom,apex, right_bottom,]], dtype=np.int32)
-        cv2.fillPoly(mask, vertices, ignore_mask_color)
-        masked_edges = cv2.bitwise_and(edges, mask)
-        rho = 1 # distance resolution in pixels of the Hough grid
-        theta = np.pi/180 # angular resolution in radians of the Hough grid
-        threshold = 2     # minimum number of votes (intersections in Hough grid cell)
-        min_line_length = 4 #minimum number of pixels making up a line
-        max_line_gap = 5    # maximum gap in pixels between connectable line segments
-        line_image = np.copy(img)*0 # creating a blank to draw lines on
-
-        # Run Hough on edge detected image
-        # Output "lines" is an array containing endpoints of detected line segments
-        lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
-                                    min_line_length, max_line_gap)
-
-        # Iterate over the output "lines" and draw lines on a blank image
+        region_of_interest = self._get_region_of_interest(img)
+        canny = self._canny(img)
+        masked_canny = self._mask_canny(canny, region_of_interest)
+        lines = self._get_hough_lines(masked_canny)
+        line_image = np.copy(img)*0
         for line in lines:
             for x1,y1,x2,y2 in line:
                 cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
-
-        # Create a "color" binary image to combine with line image
-        color_edges = np.dstack((edges, edges, edges)) 
-
         lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
-
         return lines_edges
 
     def _get_region_of_interest(self, img):
@@ -51,7 +36,26 @@ class LaneLineDetector(object):
         left_bottom = (0, y_size)
         right_bottom = (x_size, y_size)
         apex = (x_size/2, y_size/(2 * self.apex_factor))
-        return left_bottom, right_bottom, apex
+        region_of_interest = (left_bottom, apex, right_bottom)
+        return region_of_interest
     
-    def _detect_line(self):
-        pass
+    def _get_hough_lines(self, masked_edges, rho=1, theta=np.pi/180, threshold=2, min_line_length=4, max_line_gap=5):
+        lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
+                                                       min_line_length, max_line_gap)
+        return lines
+    
+    def _canny(self, img):
+        gray_img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+        blur_gray = cv2.GaussianBlur(gray_img,(self.kernel_size, self.kernel_size),0)
+        edges = cv2.Canny(blur_gray, self.low_threshold, self.high_threshold)
+        return edges
+    
+    def _mask_canny(self, canny, region_of_interest):
+        mask = np.zeros_like(canny)   
+        ignore_mask_color = 255
+        vertices = np.array([[region_of_interest[0], region_of_interest[1], region_of_interest[2],]], dtype=np.int32)
+        cv2.fillPoly(mask, vertices, ignore_mask_color)
+        
+        masked_canny = cv2.bitwise_and(canny, mask)
+        return masked_canny
+    
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
 import matplotlib.image as mpimg
 from lane_detection import LaneLineDetector
+from utils import load_config
 import cv2
 
 
 def main():
-
-    lane_line_detector = LaneLineDetector()
-    video_path = 'test_videos/solidYellowLeft.mp4'
+    config = load_config("config.yml")
+    lane_line_detector = LaneLineDetector(config)
+    video_path = config['test_video']['path']
     video_capture = cv2.VideoCapture(video_path)
     if not video_capture.isOpened():
         print("Error: Could not open video.")
@@ -16,19 +17,11 @@ def main():
     while video_capture.isOpened():
         ret, frame = video_capture.read()
         if not ret:
-            break  # Exit loop when video ends or fails to read
-
-        # Detect lane lines on the current frame
+            break  
         frame = lane_line_detector.detect(frame)
-
-        # Display the result
         cv2.imshow('Lane Line Detection', frame)
-
-        # Exit when 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-    # Release resources
     video_capture.release()
     cv2.destroyAllWindows()
 if __name__ == "__main__":
